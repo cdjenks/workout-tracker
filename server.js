802e1aab3e12baebeac6cbfac53e5140c694d073
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-07-12 12:35:17 -0400

    Modified server.js for heroku deployment

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(express.static("public"));
 app.use(require("./routes/api-routes.js"));
 require("./routes/html-routes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:dbpassword1@ds221155.mlab.com:21155/heroku_tvn8zz79", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useMongoClient: true
 });
 
 
